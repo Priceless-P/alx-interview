433f76316661c1571e91fe31f670b7dacc49f3cf
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-04-22 07:12:13 +0100

    task done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def pascal_triangle(n: str):
+    """Generate a Pascals triangle given a
+    number of row. It returns a List of List of Integers"""
+    if n <= 0:
+        return []
+    # Base case is [1]
+    elif n == 1:
+        return [[1]]
+    else:
+        triangle = pascal_triangle(n - 1)
+        last_row = triangle[-1]
+        new_row = [1]
+        for i in range(1, len(last_row)):
+            # Value of each number in a row is a sum of two
+            # numbers right and left above it.
+            new_row.append(last_row[i - 1] + last_row[i])
+        new_row.append(1)
+        triangle.append(new_row)
+        return triangle
