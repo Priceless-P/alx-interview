573855d564ea4f6e8d3735800f44a2e0465dcce2
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-05-30 19:23:08 +0100

    fix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """Rotate 2D Matrix"""
-from typing import List
 
 
-def rotate_2d_matrix(matrix: List[List]) -> None:
+def rotate_2d_matrix(matrix):
     """Rotate n x n 2D matrix 90 degrees
     clockwise"""
     n = len(matrix)
